171461fe33cb8433366cca191b2bc33835b355b4
Shashank Dhakate <shashankdkte@gmail.com>
2023-04-25 19:49:30 +0530

    changes in app.js

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import passport from "passport";
+import mongoose from "mongoose";
+import session from "express-session";
+import MongoStore from "connect-mongo";
 import { join } from "path";
 import userRouter from "./routers/userRouter";
 import globalRouter from "./routers/globalRouter";
@@ -12,20 +16,35 @@ import routes from "./routes";
 import { localMiddleware } from "./middlewares";
 
 const app = express();
-app.set("view engine", "pug");
-app.use(express.static(join(__dirname, "static")));
-app.use("/uploads", express.static("uploads"));
-// app.use("/static", express.static("static"));
+
 app.use(
   helmet({
     contentSecurityPolicy: false,
   })
 );
+app.set("view engine", "pug");
+app.use(express.static(join(__dirname, "static")));
+app.use("/uploads", express.static("uploads"));
+// app.use("/static", express.static("static"));
+
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(morgan("dev"));
+app.use(
+  session({
+    secret: process.env.COOKIE_SECRET,
+    resave: true,
+    saveUninitialized: false,
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGO_URL,
+    }),
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(localMiddleware);
 app.use(routes.users, userRouter);
 app.use(routes.home, globalRouter);
--- middlewares.js
+++ middlewares.js
@@ -5,10 +5,7 @@ const multerVideo = multer({ dest: "uploads/videos/" });
 export const localMiddleware = (req, res, next) => {
   res.locals.siteName = "VShare";
   res.locals.routes = routes;
-  res.locals.user = {
-    isAuthenticated: true,
-    id: 1,
-  };
+  res.locals.user = req.user || null;
   next();
 };
 
